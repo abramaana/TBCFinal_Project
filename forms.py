86dd635552d6a71e2dea43e78e5a2d037d0dde14
ana <abramaana912@gmail.com>
2025-07-14 12:47:38 +0400

    update1

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_wtf.file import FileField, FileSize, FileAllowed, FileRequired
+
+class RegisterForm(FlaskForm):
+    email = StringField(validators=[DataRequired(), Email()])
+    username = StringField(validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField(validators=[DataRequired(), Length(min=6, max=20)])
+    usernameL = StringField(validators=[DataRequired(), Length(min=2, max=20)])
+    passwordL = PasswordField(validators=[DataRequired(), Length(min=6, max=20)])
+    submit = SubmitField()
